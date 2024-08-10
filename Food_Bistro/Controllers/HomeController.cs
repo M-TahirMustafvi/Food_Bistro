a14d8fdd3d218c9e27688424955ad2eb8c80cab1
= <=>
2024-08-11 00:48:19 +0500

    Initial Commit

---
+++ Food_Bistro/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Bistro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("SignUp");
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string Name, string Mail, string Pwd)
+        {
+            return View("Index");
+        }
+    }
+}
