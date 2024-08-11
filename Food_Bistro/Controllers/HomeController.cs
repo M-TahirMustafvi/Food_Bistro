b21e0774992c326c526a5c61bc9d2db087740df0
= <=>
2024-08-11 17:09:18 +0500

    Some Backend FootPrints

--- Food_Bistro/Controllers/HomeController.cs
+++ Food_Bistro/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Food_Bistro.Models.Repositories;
+using Food_Bistro.Models.Classes;
 
 namespace Food_Bistro.Controllers
 {
@@ -11,9 +13,45 @@ namespace Food_Bistro.Controllers
         }
         
         [HttpPost]
-        public IActionResult Index(string Name, string Mail, string Pwd)
+        public IActionResult Index(Users usr)
         {
-            return View("Index");
+            UserRepo repo = new UserRepo();
+            if(repo.addUser(usr))
+                return View("Index");
+            ViewBag.Message = "*Email already registered!";
+            return View("SignUp");
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(String Email, String Password)
+        {
+            //If Name was added as a cooky, it means all cookies
+            //were already added
+            
+            if (!HttpContext.Request.Cookies.ContainsKey("Name"))
+            {
+                CookieOptions option = new CookieOptions();
+                option.Expires = System.DateTime.Now.AddDays(2);
+
+                if(!string.IsNullOrEmpty(Email))
+                    HttpContext.Response.Cookies.Append("mail", Email, option);
+
+                if (!string.IsNullOrEmpty(Password))
+                    HttpContext.Response.Cookies.Append("pwd", Password, option);
+            }
+
+            UserRepo repo = new UserRepo();
+            if(repo.authUser(Email, Password))
+                return View("Index");
+
+            ViewBag.Message = "*Wrong Authentication Details!";
+            return View("LogIn");
         }
     }
 }
